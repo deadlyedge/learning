dfd37c8f5aa46a63443f28f948999626a438b9ab
xdream <xdream@gmail.com>
2020-04-23 14:00:54 +0800

    write a personal logger init module.

--- init_logger.py
+++ init_logger.py
@@ -1,16 +1,20 @@
 import logging
-import sys
+from sys import stdout as display
 
 FORMAT_DISPLAY = '%(levelname) -7s - %(message)s'
 FORMAT_LOG_FILE = '%(asctime)s | %(name)s | %(levelname) -7s - %(message)s'
 FORMAT_DATETIME = '%Y/%m/%d %H:%M:%S'
+BASE_LOG_LEVEL = logging.DEBUG
+DISPLAY_LEVEL = logging.DEBUG
+FILE_LEVEL = logging.INFO
+FILE_OUTPUT = 'output.log'
 
 
 class CustomFormatter(logging.Formatter):
     """Logging Formatter to add colors and count warning / errors"""
 
     NO_COLOR = "\x1b[m"
-    RED, GREEN, ORANGE, BLUE, PURPLE, LBLUE, GREY, WHITE = \
+    RED, GREEN, ORANGE, BLUE, PURPLE, L_BLUE, GREY, WHITE = \
         map("\x1b[%dm".__mod__, range(31, 39))
 
     BOLD_RED = '\x1b[31;1m'
@@ -33,19 +37,22 @@ class CustomFormatter(logging.Formatter):
 
 def get_my_logger(mod_name):
     logger = logging.getLogger(mod_name)
-    logger.setLevel(level=logging.DEBUG)
+    logger.setLevel(BASE_LOG_LEVEL)
     formatter_file = logging.Formatter(fmt=FORMAT_LOG_FILE, datefmt=FORMAT_DATETIME)
 
     # StreamHandler
-    stream_handler = logging.StreamHandler(sys.stdout)
-    stream_handler.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler(display)
+    stream_handler.setLevel(DISPLAY_LEVEL)
     stream_handler.setFormatter(CustomFormatter())
     logger.addHandler(stream_handler)
 
     # FileHandler
-    file_handler = logging.FileHandler('output.log', encoding='utf-8')
-    file_handler.setLevel(logging.INFO)
+    file_handler = logging.FileHandler(FILE_OUTPUT, encoding='utf-8')
+    file_handler.setLevel(FILE_LEVEL)
     file_handler.setFormatter(formatter_file)
     logger.addHandler(file_handler)
+
+    # Other Handlers
+    # ...
 
     return logger
